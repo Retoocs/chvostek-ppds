4624756c6f47dcb6b4a9ace78df84b017701f427
Matej <matejchvostek@gmail.com>
2022-02-17 21:21:46 +0100

    docs: add code documentation

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD.
+        Bc. Matej Chvostek
+University:  Slovak University of Technology in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/1-cvicenie-oboznamenie-sa-s-prostredim-%f0%9f%90%8d/
+"""
+
 from collection.Collection import Counter
 from fei.ppds import Thread
 
 class Shared():
+    """
+    Saves a list of number elements. Contains counter and size attribute
+    """
     def __init__(self, size):
         self.counter = 0
         self.end = size
         self.elms = [0] * size
 
 def do_count(shared):
+    """
+    Gets an object shared, that has a list of numbers and increments every element by one
+
+    :param shared: an instance of class Shared
+    :return: none
+    """
     while True:
         if shared.counter >= shared.end:
             break
