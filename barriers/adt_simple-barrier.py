099c3e6f800762aec88d4617aefc101a87b4c061
Matej <matejchvostek@gmail.com>
2022-02-28 19:24:20 +0100

    docs: add docstrings in adt_simple-barrier

--- barriers/adt_simple-barrier.py
+++ barriers/adt_simple-barrier.py
@@ -1,3 +1,12 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD., Bc. Matej Chvostek
+University:  Slovak University of Technology in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/2-cvicenie-turniket-bariera-%f0%9f%9a%a7/?%2F
+"""
+
 from random import randint
 from time import sleep
 from fei.ppds import Thread
@@ -7,13 +16,24 @@ from fei.ppds import print
 
 
 class SimpleBarrier:
+    """
+    A synchronization object of kind barrier
+    """
     def __init__(self, n):
+        """
+        Initialization method
+        :param n: represents number of threads
+        """
         self.numberOfThreads = n
         self.counter = 0
         self.mutex = Mutex()
         self.event = Event()
 
     def wait(self):
+        """
+        Main method of barrier object. Threads wait in this function to continue
+        :return: none
+        """
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.numberOfThreads:
@@ -24,6 +44,12 @@ class SimpleBarrier:
 
 
 def barrier_example(barrier, thread_id):
+    """
+    An example function, that simulates barrier usage. It prints text before and after barrier
+    :param barrier: synchronization object
+    :param thread_id: id of current thread
+    :return:
+    """
     sleep(randint(1, 10) / 10)
     print("Thread %d is BEFORE barrier" % thread_id)
     barrier.wait()
@@ -31,6 +57,9 @@ def barrier_example(barrier, thread_id):
 
 
 if __name__ == "__main__":
+    """
+    The entry point of program. Threads are here created
+    """
     THREADS = 10
     sb = SimpleBarrier(THREADS)
     threads = [Thread(barrier_example, sb, i) for i in range(THREADS)]
