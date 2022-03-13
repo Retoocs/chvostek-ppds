a9623d81f773e9235838d5effdd0153eac4119fb
Matej <matejchvostek@gmail.com>
2022-03-13 09:25:18 +0100

    docs: add docstrings

--- nuclear_power_plant.py
+++ nuclear_power_plant.py
@@ -1,14 +1,24 @@
 from random import randint
 from time import sleep
-
 from fei.ppds import Mutex, Semaphore, Event, Thread, print
 
 class Lightswitch(object):
+    """
+    A Lightswitch synchronization object. One instance per 1 process-category
+    """
     def __init__(self):
+        """
+        Initialization method. Counter counts number of threads in 'room'
+        """
         self.mutex = Mutex()
         self.counter = 0
 
     def lock(self, sem):
+        """
+        Locks the 'room'
+        :param sem: base synchronization object of type Semaphore
+        :return: Number of threads in 'room' when calling this method
+        """
         self.mutex.lock()
         counter = self.counter
         self.counter += 1
@@ -18,6 +28,11 @@ class Lightswitch(object):
         return counter
 
     def unlock(self, sem):
+        """
+        Unlocks the 'room'
+        :param sem: base synchronization object of type Semaphore
+        :return: None
+        """
         self.mutex.lock()
         self.counter -= 1
         if self.counter == 0:
@@ -52,7 +67,14 @@ class SimpleBarrier(object):
         self.event.wait()
 
 class SychronizationHelper(object):
+    """
+    Wraps all the synchronization objects for this program.
+    """
     def __init__(self, nWriters):
+        """
+        Initialization of needed attributes
+        :param nWriters: number of threads from category 'writers'
+        """
         self.accessData = Semaphore(1)
         self.turnstile = Semaphore(1)
         self.ls_monitor = Lightswitch()
@@ -61,6 +83,12 @@ class SychronizationHelper(object):
         self.barrier = SimpleBarrier(nWriters)
 
 def monitor(monitorId, sh):
+    """
+    Simulates the monitor function. 'Readers' category. Less prioritized than sensor.
+    :param monitorId: identifier of monitor
+    :param sh: object of type SynchronizationHelper
+    :return: None
+    """
     sh.validData.wait()
     while True:
         sh.turnstile.wait()
@@ -74,6 +102,14 @@ def monitor(monitorId, sh):
 
 
 def sensor(sh, minWait, maxWait, type):
+    """
+    Simulates the sensor function. 'Writers' category. It has higher priority than monitor
+    :param sh: object of type SynchronizationHelper
+    :param minWait: minimal time in ms it takes to write data. Depends on sensor type.
+    :param maxWait: maximal time in ms it takes to write data. Depends on sensor type.
+    :param type: type of sensor
+    :return: None
+    """
     sleep(randint(50, 60) / 1000)
     writeTime = randint(minWait, maxWait) / 1000
     sleep(writeTime)
@@ -81,7 +117,7 @@ def sensor(sh, minWait, maxWait, type):
     sh.barrier.wait()
     sh.validData.signal()
     while True:
-        sleep(randint(50, 60) / 100)
+        sleep(randint(50, 60) / 1000)
         sh.turnstile.wait()
         numberOfWritingSensors = sh.ls_monitor.lock(sh.accessData)
         sh.turnstile.signal()
@@ -93,6 +129,9 @@ def sensor(sh, minWait, maxWait, type):
 
 
 if __name__ == "__main__":
+    """
+    Entry point of program. Three threads for 'writers' and 8 threads for 'readers' are created
+    """
     helper = SychronizationHelper(3)
     Thread(sensor, helper, 10, 20, "P")
     Thread(sensor, helper, 10, 20, "T")
