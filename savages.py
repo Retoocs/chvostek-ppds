d29bc5b32b569df463aecf815766fb753b436e4b
Matej <matejchvostek@gmail.com>
2022-03-20 20:06:31 +0100

    docs: add docstrings

--- savages.py
+++ savages.py
@@ -1,17 +1,37 @@
-
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD., Bc. Matej Chvostek
+University:  Slovak University of Technology in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/5-cvicenie-problem-fajciarov-problem-divochov-%f0%9f%9a%ac/
+"""
 from fei.ppds import Semaphore, Mutex, Thread, print
 from random import randint
 from time import sleep
 
 
 class SimpleBarrier:
+    """
+    Synchronization object of type Barrier
+    """
     def __init__(self, N):
+        """
+        Initialization method
+        :param N: Number of threads to stop
+        """
         self.N = N
         self.mutex = Mutex()
         self.cnt = 0
         self.sem = Semaphore(0)
 
     def wait(self, fullPot=None, last_thread_signal=False):
+        """
+        Method to block threads.
+        :param fullPot: Synchronization object of type Semaphore
+        :param last_thread_signal: If true, fullPot.signal() is called with the last thread
+        :return: None
+        """
         self.mutex.lock()
         self.cnt += 1
         if self.cnt == self.N:
@@ -24,7 +44,22 @@ class SimpleBarrier:
 
 
 class Shared:
+    """
+    Shared object to store needed variables
+    """
     def __init__(self, C):
+        """
+        Initialization method
+        :param C: Number of cooks
+
+        Attributes:
+            - mutexSavage: mutex for savage thread
+            - mutexCook: mutex for cook thread
+            - servings: actual number of servings in pot
+            - full_pot: Semaphore to signal pot is full
+            - empty_pot: Semaphore to signal pot is empty
+            - barrier1, barrier2: barriers for cook threads
+        """
         self.mutexSavage = Mutex()
         self.mutexCook = Mutex()
         self.servings = 0
@@ -35,14 +70,32 @@ class Shared:
 
 
 def get_serving_from_pot(savage_id, shared):
+    """
+    Function simulates taking portions
+    :param savage_id: savage identifier
+    :param shared: shared helper object
+    :return: None
+    """
     print("savage %2d: taking a portion" % savage_id)
     shared.servings -= 1
 
 def eat(savage_id):
+    """
+    Function simulates savage eating
+    :param savage_id: savage identifier
+    :return:
+    """
     print("savage %2d: eating" % savage_id)
     sleep(0.2 + randint(0, 3) / 10)
 
 def savage(savage_id, shared, C):
+    """
+    Function for savage thread
+    :param savage_id: Savage identifier
+    :param shared: shared helper object
+    :param C: number of cooks
+    :return: None
+    """
     while True:
         shared.mutexSavage.lock()
         print("savage %2d: portions left in pot: %2d" % (savage_id, shared.servings))
@@ -57,15 +110,30 @@ def savage(savage_id, shared, C):
 
 
 def put_servings_in_pot(M, shared, cook_id):
+    """
+    Function simulates putting portions
+    :param M: capacity of pot
+    :param shared: shared helper object
+    :param cook_id: cook identifier
+    :return: None
+    """
     print(f"cook    {cook_id}: inserting portions")
     shared.servings += M
 
 
 def cook(M, shared, cook_id):
+    """
+    Function for cook thread
+    :param M: capacity of pot
+    :param shared: shared helper object
+    :param cook_id: cook identifier
+    :return: None
+    """
     while True:
         shared.empty_pot.wait()
         print(f"cook    {cook_id}: cooking")
         sleep(randint(1, 3) / 100)
+
         shared.mutexCook.lock()
         if shared.servings == 0:
             put_servings_in_pot(M, shared, cook_id)
@@ -78,6 +146,10 @@ def cook(M, shared, cook_id):
 
 
 def main():
+    """
+    Main function of the program. Configures program and creates threads
+    :return: None
+    """
     # savages
     N = 3
     # pot capacity
@@ -97,4 +169,7 @@ def main():
 
 
 if __name__ == "__main__":
+    """
+    Entry point of program
+    """
     main()
