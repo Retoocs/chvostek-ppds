89af9cd88cf0c08e5a259761c50e0f84a4bcdbb9
Matej <matejchvostek@gmail.com>
2022-04-21 18:48:15 +0200

    docs: add docstrings

--- async.py
+++ async.py
@@ -1,13 +1,31 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD., Bc. Matej Chvostek
+University:  Slovak University of Technology in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/8-cvicenie-asynchronne-programovanie/
+"""
+
 import asyncio
 import time
 
 async def task(sleepTime):
+    """
+    Function to simulate some task. For simulation sleep() function is used.
+    :param sleepTime: delay in second for sleep()
+    :return: None
+    """
     print(f"Evaluating task for estimated {sleepTime} seconds.")
     await asyncio.sleep(sleepTime)
     print("Task finished")
 
 
 async def main():
+    """
+    Main function. Asynchronously starts tasks with params
+    :return:
+    """
     start = time.time()
     await asyncio.gather(
         task(3),
@@ -18,4 +36,7 @@ async def main():
 
 
 if __name__ == "__main__":
+    """
+    Entry point of program
+    """
     asyncio.run(main())
--- sync.py
+++ sync.py
@@ -1,12 +1,30 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD., Bc. Matej Chvostek
+University:  Slovak University of Technology in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/8-cvicenie-asynchronne-programovanie/
+"""
+
 import time
 
 def task(sleepTime):
+    """
+    Function to simulate some task. For simulation sleep() function is used.
+    :param sleepTime: delay in second for sleep()
+    :return: None
+    """
     print(f"Evaluating task for estimated {sleepTime} seconds.")
     time.sleep(sleepTime)
     print("Task finished")
 
 
 def main():
+    """
+    Main function. Creates objects of tasks and executes them with the params
+    :return: None
+    """
     tasks = [(task, 3), (task, 4), (task, 5)]
 
     start = time.time()
@@ -16,4 +34,7 @@ def main():
 
 
 if __name__ == "__main__":
+    """
+    Entry point of program
+    """
     main()
